085fb4e7be86589a25209cd48a376b23bdfe0507
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-15 14:27:42 -0500

    feat: console and i18n settings

--- configuration.nix
+++ configuration.nix
@@ -31,17 +31,12 @@
 
   time.timeZone = "America/Lima";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "es";
+    useXkbConfig = true;
+  };
 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;

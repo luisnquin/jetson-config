0c2f107dad2efa69e046aa312f209718b5eecdbe
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-15 15:03:57 -0500

    feat: setup zsh and tmux

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,8 @@
 {
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
   ];
@@ -9,14 +13,20 @@
 
   nixpkgs.config.allowUnfree = true;
 
-  hardware.nvidia-jetpack.enable = true;
-  hardware.nvidia-jetpack.som = "orin-nano";
-  hardware.nvidia-jetpack.carrierBoard = "devkit";
+  hardware = {
+    nvidia-jetpack = {
+      enable = true;
+      som = "orin-nano";
+      carrierBoard = "devkit";
+    };
 
-  hardware.graphics.enable = true;
+    graphics.enable = true;
+  };
 
-  networking.hostName = "jyx";
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = "jyx";
+    networkmanager.enable = true;
+  };
 
   time.timeZone = "America/Lima";
 
@@ -30,6 +40,69 @@
   users.users.ori = {
     isNormalUser = true;
     extraGroups = ["wheel"];
+    shell = pkgs.zsh;
+  };
+
+  programs.tmux = {
+    enable = true;
+
+    extraConfig = ''
+      unbind r
+      set-option -ga terminal-overrides ",*256col*:Tc:RGB"
+      set -g default-terminal "xterm-256color"
+      setw -g pane-base-index 200
+      set -g base-index 100
+      set-window-option -g mode-keys vi
+      set -g window-status-separator '\'
+      set -g status-interval 5
+      set -g display-time 4000
+      setw -g mouse on
+      set -g terminal-overrides 'xterm*:smcup@:rmcup@'
+      setw -g aggressive-resize off
+      setw -g clock-mode-style 24
+      set -s escape-time 500
+      set -g history-limit 1000000
+    '';
+  };
+
+  programs.zsh = {
+    enable = true;
+    autosuggestions = {
+      enable = true;
+      async = true;
+
+      highlightStyle = "fg=#9eadab";
+      strategy = [
+        "history"
+      ];
+    };
+
+    syntaxHighlighting = {
+      enable = true;
+    };
+
+    enableBashCompletion = true;
+    enableCompletion = true;
+
+    promptInit = ''
+      if [ "$TMUX" = "" ]; then
+          exec ${lib.getExe pkgs.tmux}
+      fi
+    '';
+
+    interactiveShellInit = ''
+      export GID UID
+
+      # This keybindings allows for fast navigation from left to right and back.
+      bindkey '^[[1;5D' backward-word
+      bindkey '^[[1;5C' forward-word
+
+      # Case-less when searching for files/directories
+      zstyle ':completion:*' matcher-list '\' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+
+      # https://stackoverflow.com/a/11873793
+      setopt interactivecomments
+    '';
   };
 
   programs.git = {

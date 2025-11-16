6c7df49bb6ac21acc5a8db085f89639782699fd4
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-16 12:28:47 -0500

    refactor: new module for only terminal and shell config

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,9 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ./hardware-configuration.nix
     ./disko-config.nix
     ./components/i3.nix
+    ./components/term+shell.nix
   ];
 
   nixpkgs.config = {
@@ -42,11 +39,6 @@
   time.timeZone = "America/Lima";
 
   i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-    font = "Lat2-Terminus16";
-    keyMap = "es";
-    useXkbConfig = false;
-  };
 
   users.users.ori = {
     isNormalUser = true;
@@ -54,86 +46,10 @@
     shell = pkgs.zsh;
   };
 
-  programs.tmux = {
-    enable = true;
-
-    extraConfig = ''
-      unbind r
-      set-option -ga terminal-overrides ",*256col*:Tc:RGB"
-      set -g default-terminal "xterm-256color"
-      setw -g pane-base-index 200
-      set -g base-index 100
-      set-window-option -g mode-keys vi
-      set -g window-status-separator '\'
-      set -g status-interval 5
-      set -g display-time 4000
-      setw -g mouse on
-      set -g terminal-overrides 'xterm*:smcup@:rmcup@'
-      setw -g aggressive-resize off
-      setw -g clock-mode-style 24
-      set -s escape-time 500
-      set -g history-limit 1000000
-    '';
-  };
-
   environment = {
     systemPackages = [pkgs.xclip];
     shellAliases = {
       copy = "xclip -selection clipboard";
-    };
-  };
-
-  programs.zsh = {
-    enable = true;
-    autosuggestions = {
-      enable = true;
-      async = true;
-
-      highlightStyle = "fg=#9eadab";
-      strategy = [
-        "history"
-      ];
-    };
-
-    syntaxHighlighting = {
-      enable = true;
-    };
-
-    enableBashCompletion = true;
-    enableCompletion = true;
-
-    promptInit = ''
-      if [ "$TMUX" = "" ]; then
-          exec ${lib.getExe pkgs.tmux}
-      fi
-    '';
-
-    interactiveShellInit = ''
-      export GID UID
-
-      # This keybindings allows for fast navigation from left to right and back.
-      bindkey '^[[1;5D' backward-word
-      bindkey '^[[1;5C' forward-word
-
-      # Case-less when searching for files/directories
-      zstyle ':completion:*' matcher-list '\' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
-
-      # https://stackoverflow.com/a/11873793
-      setopt interactivecomments
-    '';
-  };
-
-  programs.git = {
-    enable = true;
-    config = {
-      user = {
-        email = "luis@quinones.pro";
-        name = "Luis Quiñones";
-      };
-      init.defaultBranch = "main";
-      url."ssh://git@github.com/".insteadOf = "https://github.com/";
-      rebase.autoStash = true;
-      pull.rebase = true;
     };
   };
 

c0a2521d62a910bcb72862f97860a88748473aee
Luis Quiñones <lpaandres2020@gmail.com>
2025-11-16 19:51:34 -0500

    feat(i3): disable wm for now

--- components/i3.nix
+++ components/i3.nix
@@ -1,22 +1,22 @@
-{pkgs, ...}: {
+{
   environment.pathsToLink = ["/libexec"];
 
   services.xserver = {
     enable = true;
 
-    desktopManager = {
-      xterm.enable = false;
-    };
+    # desktopManager = {
+    #   xterm.enable = false;
+    # };
 
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3blocks
-      ];
-    };
+    # windowManager.i3 = {
+    #   enable = true;
+    #   extraPackages = with pkgs; [
+    #     dmenu
+    #     i3status
+    #     i3blocks
+    #   ];
+    # };
   };
 
-  services.displayManager.defaultSession = "none+i3";
+  # services.displayManager.defaultSession = "none+i3";
 }

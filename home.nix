630e3b1b603aea91105eb856a7a78892f1d43eec
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-15 21:42:24 -0500

    feat: enable black terminal modules @ hm level

--- home.nix
+++ home.nix
@@ -5,4 +5,26 @@
     stateVersion = "26.05";
     enableNixpkgsReleaseCheck = true;
   };
+
+  shared = {
+    bat.enable = true;
+    btop.enable = true;
+    direnv.enable = true;
+    eza.enable = true;
+    fzf.enable = true;
+    git = {
+      enable = true;
+      user = {
+        name = "Luis Quiñones";
+        email = "luis@quinones.pro";
+      };
+    };
+    lazygit.enable = true;
+    macchina.enable = true;
+    magic-wormhole.enable = true;
+    starship.enable = true;
+    tmux.enable = true;
+    zoxide.enable = true;
+    zsh.enable = true;
+  };
 }

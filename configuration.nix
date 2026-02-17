e733f348b60c794e6228cdee5dd0223c83d64b89
Luis Quiñones <lpaandres2020@gmail.com>
2026-02-17 18:33:48 -0500

    feat: set console.keyMap to es

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,8 @@
     zsh.enable = true;
   };
 
+  console.keyMap = "es";
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
